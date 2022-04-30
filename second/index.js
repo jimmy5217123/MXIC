8bc1a8db29626b204981758c1f4392b909902518
jimmy5217123 <66046776+jimmy5217123@users.noreply.github.com>
2022-05-01 01:41:32 +0800

    create second exam

---
+++ second/index.js
@@ -0,0 +1,13 @@
+import { MainGroup, SubGroup } from './tree';
+var app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+        MainGroup,
+        SubGroup
+    },
+    computed: {
+    },
+    methods: {
+    }
+})
